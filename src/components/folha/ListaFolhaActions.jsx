51e3e762b65e4db90ea7f64add34b158051c262e
Ceneged Supervisão - SP <rondinelerac@gmail.com>
2025-10-16 16:54:22 -0300

    expt

--- src/components/folha/ListaFolhaActions.jsx
+++ src/components/folha/ListaFolhaActions.jsx
@@ -32,7 +32,8 @@ const FolhaActions = ({
 
   const exportDisabled =
     isExporting ||
-    (isBackoffice && ["rascunho", "aguardando_correcao"].includes(folha.status));
+    //(isBackoffice && ["rascunho", "aguardando_correcao"].includes(folha.status));
+    (!isBackoffice && ["rascunho", "aguardando_correcao"].includes(folha.status));
 
   return (
     <div className="flex flex-nowrap overflow-x-auto gap-1 py-1  justify-left">
